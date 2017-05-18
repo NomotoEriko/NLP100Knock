f0cbd07a40c6eeea4d51410af7c96a0932d51dc1
NomotoEriko <nomoto.eriko@ist.osaka-u.ac.jp>
2017-05-18 09:21:31 +0900

    Update q13.py
    debug

--- python/section2/q13.py
+++ python/section2/q13.py
@@ -5,5 +5,5 @@ if __name__ == '__main__':
             cols.append(f.readlines())
     with open('col12.txt', 'w') as fw:
         for row in zip(*cols*1):
-            row = [r.strip() for r in row]
+            row = ['\t'.join(r.strip().split('\t')[:2]) for r in row]
             fw.write('\t'.join(row)+'\n')
