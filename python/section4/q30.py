1a863cd22180ccd45f3cd835d42e2182c397fa53
野本　英梨子 <nomotoeriko@nomotoeinashi-no-MacBook-Pro.local>
2017-06-04 20:40:17 +0900

    modified

--- python/section4/q30.py
+++ python/section4/q30.py
@@ -6,7 +6,7 @@ def load_neko():
             surface, elems = line.split('\t')
             elems = elems.split(',')
             morpheme = {
-                'surface': surface.strip(), 'base': elems[6].strip(), 'pos': elems[0].strip(), 'pos1': elems[1].strip()
+                'surface': surface, 'base': elems[6], 'pos': elems[0].strip(), 'pos1': elems[1].strip()
             }
             sentence.append(morpheme)
         else:
@@ -18,4 +18,4 @@ def load_neko():
 if __name__ == '__main__':
     neko = load_neko()
     from pprint import pprint
-    pprint(neko[:100])
+    pprint(neko[:2])
