29db035e762b8689f137a5c7b37ce2f8e4e652b4
野本　英梨子 <nomotoeriko@nomotoeinashi-no-MacBook-Pro.local>
2017-06-08 14:04:04 +0900

    修正

--- python/section2/q12.py
+++ python/section2/q12.py
@@ -1,7 +1,8 @@
 from q10 import load_pure_hightmp
 
 if __name__ == '__main__':
-    cols = load_pure_hightmp()[:2]
+    cols = load_pure_hightmp()
     for i in 1, 2:
         with open('col%d.txt' % i, 'w') as f:
-             print(cols[i-1], file=f)
+            for col in cols:
+                print(col.split("\t")[i-1], file=f)
